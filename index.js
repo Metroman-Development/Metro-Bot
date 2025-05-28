90d83a4d416f380f62a6e490568e29a846a08992
MetroMan <services@metroman.me>
2025-05-28 17:49:37 -0400

    Ya casi index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 const { readdirSync } = require('fs');
 
