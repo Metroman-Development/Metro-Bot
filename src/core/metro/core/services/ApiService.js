51b86dd4cae252e4adaa736897f61dec91f62524
MetroMan <services@metroman.me>
2025-08-18 17:04:42 -0400

    Update ApiService.js

--- src/core/metro/core/services/ApiService.js
+++ src/core/metro/core/services/ApiService.js
@@ -1069,6 +1069,9 @@ async activateEventOverrides(eventDetails) {
 
         const stationsArray = Array.isArray(dbStations) ? dbStations : Object.values(dbStations);
 
+
+        console.log(stationsArray)
+        
         for (const station of stationsArray) {
             const lineId = station.line_id.toLowerCase();
             if (dbRawData.lineas[lineId]) {
@@ -1085,7 +1088,7 @@ async activateEventOverrides(eventDetails) {
             }
         }
 
-        console.log(dbRawData.lineas['l1'].estaciones);
+        //console.log(dbRawData.lineas['l1'].estaciones);
         
 
         return dbRawData;
